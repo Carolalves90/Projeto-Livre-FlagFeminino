af39be2689c0c90b44f3cf19714a5e28ebe18466
Carol Alves <prof.carolalves90@gmail.com>
2022-07-15 14:41:08 -0300

    :bricks: criando rotas para seleção

---
+++ src/routes/selecaofemininoRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/selecaofemininoController')
+
+//router.get('/selecao', controller.historyCreate)
+// router.get('/selecao/timeline', controller.)
+// router.get('/selecao/proximosjogos', controller.)
+
+
+
+module.exports = router
