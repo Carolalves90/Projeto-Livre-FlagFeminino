42f3b9ae993d194b57523e093fc8bba51d03ee62
Carol Alves <prof.carolalves90@gmail.com>
2022-07-15 18:57:55 -0300

    :lock: código de autorização para cadastro

--- src/controller/timesfemininosController.js
+++ src/controller/timesfemininosController.js
@@ -5,18 +5,43 @@ const SECRET = process.env.SECRET
 
 const teamCreate = async (req, res) => {
     try{
-        const {nome, cidade, estado, redesocial, contato, modalidades} = req.body
-        const newTeam = new TimesfemininosModel({
-            nome, cidade, estado, redesocial, contato, modalidades
-        })
-        const savedTeam = await newTeam.save()
-    
-        res.status(201).json(savedTeam)
-    } catch {
+        const authHeader = req.get('authorization')
 
+        if(!authHeader){
+            return res.status(401).send('É necesssário um token')
+        }
+            const token = authHeader.split(' ')[1]
+
+            await jwt.verify(token, SECRET, async function (erro){
+                if (erro){
+                    return res.status(403).send('Você não está autorizado')
+                }
+                const {nome, cidade, estado, redesocial, contato, modalidades} = req.body
+                const newTeam = new TimesfemininosModel({
+                    nome, cidade, estado, redesocial, contato, modalidades
+                })
+                const savedTeam = await newTeam.save()
+        
+                res.status(201).json(savedTeam)
+            })
+    } catch (error){
+        console.error(error)
+        res.status(500).json({message: error.message})
+    }
+}
+
+const findAllTeams = async (req, res) => {
+    try{
+        const allTeams = await TimesfemininosModel.find()
+        res.status(200).json(allTeams)
+
+    } catch (error){
+        console.error(error)
+        res.status(500).json({ message: error.message})
     }
 }
 
 module.exports = {
-    teamCreate
+    teamCreate,
+    findAllTeams
 }
