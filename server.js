825a0ca41f282162ea70b83239b22467d568587d
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 17:31:01 -0300

    :bug: removendo alterações

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const app = require('./src/app.js')
-const { resolve } = require('path')
-const express = require('express')
+const app = require('./src/app')
 const PORT = process.env.PORT
-
-app.use('/',
-    express.static(
-        resolve(
-            __dirname,
-            './build'
-        )
-    )
-)
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index'));
-})
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado na porta ${PORT}`)
