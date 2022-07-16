e2a2a8b03693797620d9c27ed406a62a908d4d68
Carol Alves <prof.carolalves90@gmail.com>
2022-07-15 21:04:30 -0300

    :bricks: rota para atualizar um time

--- src/routes/timesfemininosRoutes.js
+++ src/routes/timesfemininosRoutes.js
@@ -4,5 +4,7 @@ const controller = require('../controller/timesfemininosController')
 
 router.post('/timesfemininos', controller.teamCreate)
 router.get('/timesfemininos', controller.findAllTeams)
+router.put('/timesfemininos/:id', controller.updateTeam)
+//router.delete('/timesfemininos/:id', controller.deleteTeam)
 
 module.exports = router
