d3e320492494808508c1d0fbcf8e3b7cf29b1050
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 03:03:14 -0300

    :bricks: rota para cadastro de campeonato

--- src/routes/selecaofemininoRoutes.js
+++ src/routes/selecaofemininoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/selecaofemininoController')
 
-//router.post('/selecao', controller.historyCreate)
+router.post('/selecao', controller.createInfo)
 // router.get('/selecao', controller.)
 // router.put('/selecao', controller.)
 // router.delete('/selecao', controller.)
