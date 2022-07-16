14b7d3dc332d20485baa6aff65ac96b9f6991c74
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 17:07:49 -0300

    :bug: server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./src/app')
-
-const PORT = process.env.PORT || 2305
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado na porta ${PORT}`)
