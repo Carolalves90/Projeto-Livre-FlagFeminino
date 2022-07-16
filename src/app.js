1498839811481e34e7c581d8e6daf0e1b4cb23c8
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 16:32:27 -0300

    :bug: app

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 mongoose.connect()
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(index)
+app.use('/', index)
 app.use(colaboradorasRoutes)
 app.use(timesfemininosRoutes)
 
