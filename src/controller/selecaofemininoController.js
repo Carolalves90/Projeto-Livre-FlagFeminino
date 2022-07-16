381ac412bb6a818a98e281e9d15a4165bd10b76e
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 15:16:54 -0300

    :bug: comentando erro

--- src/controller/selecaofemininoController.js
+++ src/controller/selecaofemininoController.js
@@ -2,27 +2,27 @@ const SelecaofemininaModel = require('../model/selecaofemininaModel')
 const jwt = require('jsonwebtoken')
 const SECRET = process.env.SECRET
 
-const createInfo = async(req, res) => {
-    try{
-        const authHeader = req.get('authorization')
+// const createInfo = async(req, res) => {
+//     try{
+//         const authHeader = req.get('authorization')
 
-        if(!authHeader) {
-            return res.status(401).send('É necessário um Token')
-        }
-            const token = authHeader.split(' ')[1]
+//         if(!authHeader) {
+//             return res.status(401).send('É necessário um Token')
+//         }
+//             const token = authHeader.split(' ')[1]
 
-            await jwt.verify(token, SECRET, async function(error){
-                if(error){
-                    return res.status(403).send('Token inválido')
-                }
-                // codigo aqui
-            })
-    }catch (error){
-        console.log(error)
-        res.status(500).json({message: error.message})
-    }
-}
+//             await jwt.verify(token, SECRET, async function(error){
+//                 if(error){
+//                     return res.status(403).send('Token inválido')
+//                 }
+//                 // codigo aqui
+//             })
+//     }catch (error){
+//         console.log(error)
+//         res.status(500).json({message: error.message})
+//     }
+// }
 
 module.exports ={
-    createInfo
+    //createInfo
 }
