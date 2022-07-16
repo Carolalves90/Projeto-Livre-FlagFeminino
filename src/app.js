3bb9019ebae08a243ec286f1a06a37c38a4a8dd8
Carol Alves <prof.carolalves90@gmail.com>
2022-07-16 14:46:02 -0300

    :sparkles: swagger rodando em app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const index = require('./routes/index')
 const timesfemininosRoutes = require('./routes/timesfemininosRoutes')
 const colaboradorasRoutes = require('./routes/colaboradorasRoutes')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
+
 const app = express()
 
 app.use(express.json())
@@ -14,5 +17,6 @@ mongoose.connect()
 app.use(index)
 app.use(colaboradorasRoutes)
 app.use(timesfemininosRoutes)
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app
