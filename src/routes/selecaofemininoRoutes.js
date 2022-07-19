ab2e7a99f14c79313bdf581c2b3ed354d8f6d0d6
Carol Alves <prof.carolalves90@gmail.com>
2022-07-19 17:07:46 -0300

    :bricks: rota para buscar campeonatos

--- src/routes/selecaofemininoRoutes.js
+++ src/routes/selecaofemininoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require('../controller/selecaofemininoController')
 
 router.post('/selecao', controller.createInfo)
-// router.get('/selecao', controller.)
+router.get('/selecao', controller.getAll)
 // router.put('/selecao', controller.)
 // router.delete('/selecao', controller.)
 
