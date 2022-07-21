e6f440df7463532d359078a52c82d2e691c76ec9
Carol Alves <prof.carolalves90@gmail.com>
2022-07-21 13:07:49 -0300

    :bricks: rota para buscar campeonato por id

--- src/routes/selecaofemininoRoutes.js
+++ src/routes/selecaofemininoRoutes.js
@@ -5,6 +5,7 @@ const controller = require('../controller/selecaofemininoController')
 router.post('/selecao', controller.createInfo)
 //router.post('/selecao/:id', controller.createGame)
 router.get('/selecao', controller.getAll)
+router.get('/selecao/:id', controller.findInfoById)
 router.put('/selecao/:id', controller.updateInfo)
 router.delete('/selecao/:id', controller.deleteInfo)
 
