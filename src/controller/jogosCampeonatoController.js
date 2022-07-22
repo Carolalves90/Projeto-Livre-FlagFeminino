cfd6cceecadda483c465f832ed4b19c60bf50128
Carol Alves <prof.carolalves90@gmail.com>
2022-07-22 01:32:19 -0300

    :lock: protegendo código para cadastrar jogo

--- src/controller/jogosCampeonatoController.js
+++ src/controller/jogosCampeonatoController.js
@@ -4,13 +4,25 @@ const SECRET = process.env.SECRET
 
 const registerGame = async(req, res) => {
     try{
-        const {jogoNumero, paisContra, fase, placar, localJogo, dataCalendario, diaSemana, horario} = req.body
-        const newGame = new JogosCampeonatoModel({
-            jogoNumero, paisContra, fase, placar, localJogo, dataCalendario, diaSemana, horario
-        })
-        const savedGame = await newGame.save()
+        const authHeader = req.get('authorization')
+        if(!authHeader){
+            return res.status(401).send('É necessário um Token')
+        }
 
-        res.status(201).json(savedGame)
+        const token = authHeader.split(' ')[1]
+
+        await jwt.verify(token, SECRET, async function(erro){
+            if(erro){
+                return res.status(403).send('Token inválido')
+            }
+            const {jogoNumero, paisContra, fase, placar, localJogo, dataCalendario, diaSemana, horario} = req.body
+            const newGame = new JogosCampeonatoModel({
+            jogoNumero, paisContra, fase, placar, localJogo, dataCalendario, diaSemana, horario
+            })
+            const savedGame = await newGame.save()
+
+            res.status(201).json(savedGame)
+        })        
     } catch (erro) {
         console.error(erro)
         res.status(500).json({message: error.message})
