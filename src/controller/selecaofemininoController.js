f9b0bc81fffabab37c800474d2900cae98bd56a2
Carol Alves <prof.carolalves90@gmail.com>
2022-07-21 22:59:31 -0300

    :bricks: criando codigo para cadastra jogo em um Campeonato

--- src/controller/selecaofemininoController.js
+++ src/controller/selecaofemininoController.js
@@ -1,6 +1,6 @@
 const SelecaofemininaModel = require('../model/selecaofemininaModel')
+const JogosCampeonatoModel = require('../model/jogosCampeonatoModel')
 const jwt = require('jsonwebtoken')
-const { response } = require('../app')
 const SECRET = process.env.SECRET
 
 const createInfo = async(req, res) => {
@@ -16,11 +16,9 @@ const createInfo = async(req, res) => {
                 if(error){
                     return res.status(403).send('Token inválido')
                 }
-                const {campeonato, local, ano, colocacaoFinal, chavePaises, treinadores, headCoach, ataqueCoach, defesaCoach, auxiliares, atletasRelacionadas, 
-                    jogosCampeonato, jogoNumero, paisContra, fase,placar, localJogo, dataCalendario, diaSemana, horario} = req.body
+                const {campeonato, local, ano, colocacaoFinal, chavePaises, treinadores, headCoach, ataqueCoach, defesaCoach, auxiliares, atletasRelacionadas} = req.body
                 const newInfo = new SelecaofemininaModel({
-                    campeonato, local, ano, colocacaoFinal, chavePaises, treinadores, headCoach, ataqueCoach, defesaCoach, auxiliares, atletasRelacionadas, 
-                    jogosCampeonato, jogoNumero, paisContra, fase,placar, localJogo, dataCalendario, diaSemana, horario
+                    campeonato, local, ano, colocacaoFinal, chavePaises, treinadores, headCoach, ataqueCoach, defesaCoach, auxiliares, atletasRelacionadas
                 })
                 const savedInfo = await newInfo.save()
 
@@ -34,7 +32,7 @@ const createInfo = async(req, res) => {
 
 const getAll = async (req, res) => {
     try{
-        const allInfo = await SelecaofemininaModel.find()
+        const allInfo = await SelecaofemininaModel.find().populate('jogosCampeonato')
         res.status(200).json(allInfo)
 
     }catch (error){
@@ -66,7 +64,8 @@ const updateInfo = async ( req, res) => {
                 res.status(200).json(updatedInfo)
             })
     } catch (error) {
-
+        console.error(error)
+        res.status(500).json({message: error.message})
     }
 }
 
@@ -84,22 +83,11 @@ const deleteInfo = async (req, res) => {
                 }
                 const { id } = req.params
                 await SelecaofemininaModel.findByIdAndDelete(id)
-                res.status(200).json({message: `Campeonato com o id ${id} deletado com sucesso`})
+                res.status(200).json({message: `Informação de Campeonato com o id ${id} deletado com sucesso`})
             })    
     } catch (error) {
         console.error(error)
         res.status(500).json({message: error.message})
-    }
-}
-
-const createGame = async (req, res) => {
-    try{
-        const { id } = req.params
-        const idRequest = await SelecaofemininaModel.findById(id)
-
-        // quando achar o id cadastrar um novo array de jogos
-    } catch (error) {
-
     }
 }
 
@@ -113,11 +101,47 @@ const findInfoById = async (req, res) => {
     }
 }
 
-module.exports ={
+const registerGame = async (req, res) => {
+    try{
+        const authHeader = req.get('authorization')
+        if(!authHeader){
+            return res.status(401).send('É necessário um Token')
+        }
+
+        const token = authHeader.split(' ')[1]
+
+        await jwt.verify(token, SECRET, async function (erro){
+            if(erro){
+                return res.status(403).send('Token inválido')
+            }
+                const idRequest = req.params.id
+                const findCampeonato = await SelecaofemininaModel.findById(idRequest)
+
+                if(findCampeonato == null){
+                    return res.status(404).json({message: 'Informação de Campeonato não encontrado'})
+                }
+
+                const idRequestGame = req.body.id
+                const findGame = await JogosCampeonatoModel.findById(idRequestGame)
+
+                findCampeonato.jogosCampeonato.push(findGame)
+                
+
+                const savedGame = await findCampeonato.save()
+
+                res.status(201).json(savedGame)
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {
     createInfo,
     getAll,
     updateInfo,
     deleteInfo,
-    createGame,
-    findInfoById
+    findInfoById,
+    registerGame
 }
