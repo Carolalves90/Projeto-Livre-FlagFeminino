51bd62dc35260a701ff03539994698689dde46ff
Carol Alves <prof.carolalves90@gmail.com>
2022-07-23 00:59:38 -0300

    :bricks: rota para deletar jogo

--- src/routes/jogosCampeonatoRoutes.js
+++ src/routes/jogosCampeonatoRoutes.js
@@ -5,6 +5,6 @@ const controller = require('../controller/jogosCampeonatoController')
 router.post('/jogos', controller.registerGame)
 router.get('/jogos', controller.getAllGames)
 router.put('/jogos/:id', controller.updateGame)
-// router.delete('/jogos/:id', controller.deleteGame)
+router.delete('/jogos/:id', controller.deleteGame)
 
 module.exports = router
